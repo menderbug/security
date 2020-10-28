ca1ebda6cdf9cfcd01d677e7958cef74bd039fd7
David Ye <dly8@case.edu>
2020-10-27 20:19:52 -0400

    wrote encrypt
    Signed-off-by: David Ye <dly8@case.edu>

--- Vigenere.java
+++ Vigenere.java
@@ -1,4 +1,12 @@
 
 public class Vigenere {
-
+	
+	public static String encrypt(String message, String key) {
+		StringBuilder sb = new StringBuilder();
+		key = key.toLowerCase();
+		for (int i = 0; i < message.length(); i++)
+			sb.append(message.charAt(i) + key.charAt(i % key.length()) - 'a');
+		return sb.toString();
+	}
+	
 }
