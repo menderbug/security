82129d57870e9db5561585c3fc532e4e3acecbf9
David Ye <dly8@case.edu>
2020-11-07 05:06:17 -0500

    adding the gui and some work on vigenere
    Signed-off-by: David Ye <dly8@case.edu>

---
+++ GUI.java
@@ -0,0 +1,59 @@
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class GUI extends Application {
+	
+	private GridPane board = new GridPane();
+	
+	public static void main(String[] args) {launch(args);}
+
+	public void start(Stage primaryStage) throws Exception {
+		
+		Scene primaryScene = new Scene(board);
+		primaryStage.setScene(primaryScene);
+		
+
+		TextArea input = new TextArea("it was her habit to build up laughter out of inadequate materials");
+		TextArea key = new TextArea("steinbeck");
+		TextArea output = new TextArea(Vigenere.encrypt(input.getText(), key.getText()));
+		input.textProperty().addListener((observable, oldValue, newValue) -> {
+			output.setText(Vigenere.encrypt(input.getText(), key.getText()));
+		});
+		key.textProperty().addListener((observable, oldValue, newValue) -> {
+			output.setText(Vigenere.encrypt(input.getText(), key.getText()));
+		});
+		output.textProperty().addListener((observable, oldValue, newValue) -> {
+			input.setText(Vigenere.decrypt(output.getText(), key.getText()));
+		});
+		
+		board.add(new TextField("Vigenere Encryption"), 0, 0, 3, 1);
+		board.add(new TextField("Text"), 0, 1);
+		board.add(new TextField("Key"), 1, 1);
+		board.add(new TextField("Encrypted Text"), 2, 1);
+		
+		board.add(input, 0, 2);
+		board.add(key, 1, 2);
+		board.add(output, 2, 2);
+		
+		board.getChildren().forEach(n -> {if (n instanceof TextField) ((TextField) n).setEditable(false);});
+		board.getChildren().forEach(n -> {if (n instanceof TextArea) ((TextArea) n).setWrapText(true);;});
+
+		
+		//primaryStage.setTitle("Encoder/Decoder");
+		primaryStage.initStyle(StageStyle.UNDECORATED);
+		primaryScene.getStylesheets().add(getClass().getResource("gui.css").toExternalForm());
+		//primaryStage.sizeToScene();
+		primaryStage.setResizable(false);
+		primaryStage.show();
+		
+		
+	}
+
+
+}
--- Vigenere.java
+++ Vigenere.java
@@ -1,28 +1,27 @@
+import java.util.function.BiFunction;
 
 public class Vigenere {
 	
 	public static void main(String[] args) {
-		System.out.println(Vigenere.encrypt("midterm", "eecs"));
+		System.out.println(Vigenere.decrypt("qmflivo", "eecs"));
 	}
 	
 	public static String encrypt(String message, String key) {
-		StringBuilder sb = new StringBuilder();
+		return shift(message, key, (original, shift) -> (char) ((original + shift - (2 * 'a')) % 26 + 'a'));
+	}
+	
+	public static String decrypt(String message, String key) {
+		return shift(message, key, (original, shift) -> (char) ((original - shift + 26) % 26 + 'a'));
+	}
+
+	
+	public static String shift(String message, String key, BiFunction<Character, Character, Character> bf) {
+		StringBuilder sb = new StringBuilder(message);
 		key = key.toLowerCase();
 		for (int i = 0; i < message.length(); i++)
-			sb.append(shift(message.charAt(i), key.charAt(i % key.length())));
+			if (Character.isLetter(sb.charAt(i)))
+				sb.setCharAt(i, bf.apply(sb.charAt(i), key.charAt(i % key.length())));		//TODO fix the fact that it increments on non letters
 		return sb.toString();
 	}
-	
-	public static String decypt(String message, String key) {
-		StringBuilder sb = new StringBuilder();
-		key = key.toLowerCase();
-		for (int i = 0; i < message.length(); i++)
-			sb.append(shift(message.charAt(i), key.charAt(i % key.length())));	//TODO this doesnt work
-		return sb.toString();
-	}
-	
-	private static char shift(char original, char shift) {
-		return (char) ((original + shift - (2 * 'a')) % 26 + 'a');
-	}
-	
+
 }
