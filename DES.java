b43de9db391b74bbca6e832232ccc8ad13d754bc
Caroline Zhu <caroline.yy.zhu@gmail.com>
2020-11-25 10:17:11 -0500

    Added BitSet conversion to DES

--- DES.java
+++ DES.java
@@ -11,14 +11,11 @@ public class DES {
     private final int[] numShifts = {};
     private int iteration;
 
-    public DES() {
-        this.iteration = 0;
-    }
 
     //TODO: write encode method
-    public BitSet encrypt(String encryptText) {
+    /*public BitSet encrypt(String encryptText) {
         this.iteration = 0;
-        String[] blockStrings = chunk64Bits(encryptText);
+        String[] blockStrings = stringTo64Bits(encryptText);
         return new BitSet();
     }
 
@@ -52,7 +49,6 @@ public class DES {
 
     }
 
-
     //Deepa
     private BitSet leftCircularShift (BitSet bits,  int roundnum){
         //can pull the amount of shifts to make from this.numShifts
@@ -63,18 +59,20 @@ public class DES {
         //do the 16 rounds and change value of this.subKeys;
     }
 
+    //Caroline
     //each round of DES (16 rounds)
     private BitSet round(BitSet bits, int roundnum){
     }
+    */
 
     /**
      * Takes input string and breaks it into blocks of 64-bits, stored in a String array where each entry represents 64-bits
      * @param inputString
      * @return String array with each entry having 8 characters
      */
-    private String[] stringTo64Bits(String inputString) {
+    private BitSet[] stringTo64Bits(String inputString) {
         int length = (inputString.length() / 8) + 1;
-        String[] output = new String[length];
+        BitSet[] output = new BitSet[length];
         for(int i = 0; i < output.length; i++) {
             output[i] = stringToBitString(inputString.substring(i * 8, (i + 1) * 8));
         }
@@ -82,29 +80,36 @@ public class DES {
     }
 
     /**
-     * Converts inputString into a char array
+     * Converts inputString into a BitSet by taking each character, changing it to its ASCII value, and then adding
+     * leading zeroes so that each character is represented as an 8-bit binary number
      * @param inputString
      * @return a String as represented in binary via ASCII values of each character (64 digits long)
      */
-    private String stringToBitString(String inputString) {
+    private static BitSet stringToBitString(String inputString) {
+        BitSet bitset = new BitSet();
         StringBuilder sbr = new StringBuilder();
-        for(char c : inputString.toCharArray()) {
-            int asciiValue = c;
-            String binaryValue = Integer.toBinaryString(asciiValue);
-            if(binaryValue.length() < 8) {
-                for(int j = 0; j < 8 - binaryValue.length(); j++) {
-                    sbr.append("0");
-                }
+        for (char c : inputString.toCharArray()) {
+            String binaryValue = Integer.toBinaryString(c);
+            //add leading zeroes to numbers that have fewer than 8 bits
+            if (binaryValue.length() < 8) {
+                sbr.append("0".repeat(8 - binaryValue.length()));
             }
             sbr.append(binaryValue);
         }
-
-        return sbr.toString();
+        System.out.println(sbr.toString());
+        for(int i = 0; i < sbr.length(); i++) {
+            if(sbr.toString().charAt(i) == '1') {
+                bitset.set(i);
+            }
+        }
+        return bitset;
     }
 
+    public static void main(String[] args) {
+        System.out.println(stringToBitString("hello").toString());
+    }
 
     //caroline
-    //TODO: write method
     /**
      * f function, which applies a 48-bit key to the right 32 bits to produce a 32 bit output
      * @return
