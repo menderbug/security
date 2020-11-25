6ae9e9c004f098e58bc17a07d7cff15e627cc242
Caroline Zhu <caroline.yy.zhu@gmail.com>
2020-11-24 21:02:08 -0500

    Established DES class with basic method signatures

--- DES.java
+++ DES.java
@@ -1,15 +1,28 @@
-import java.util.ArrayList;
+package security;
+
 import java.util.BitSet;
-import java.util.List;
 
 public class DES {
+    //static
+    //TODO: copy in all of the permutation tables + half s-boxes
     private final int[][] pbox = {};
     private final int[][] sbox = {};
+    private BitSet[] subKeys;
+    private final int[] numShifts = {};
+    private int iteration;
+
+    public DES() {
+        this.iteration = 0;
+    }
 
     //TODO: write encode method
     public BitSet encrypt(String encryptText) {
-
+        this.iteration = 0;
+        String[] blockStrings = chunk64Bits(encryptText);
         return new BitSet();
+    }
+
+    public BitSet encrypt(BitSet bits) {
     }
 
     //TODO: write decrypt method
@@ -17,33 +30,80 @@ public class DES {
         return "";
     }
 
-    //TODO: write method
+    //caroline
+    private BitSet generateKey() {
+
+    }
+
+    //caroline
+    private BitSet permutation(BitSet bits, int[][] box) {
+    }
+
+    //caroline
+    private BitSet reversePermutation(BitSet bits, int[][] box) {
+    }
+
+    //Deepa
+    private BitSet sboxTransform(BitSet bits, int[][] sbox) {
+    }
+
+    //Deepa
+    private reverseSBoxTransform(BitSet bits, int[][] sbox) {
+
+    }
+
+
+    //Deepa
+    private BitSet leftCircularShift (BitSet bits,  int roundnum){
+        //can pull the amount of shifts to make from this.numShifts
+    }
+
+    //Deepa
+    private BitSet[] generateSubkeys(BitSet bits){
+        //do the 16 rounds and change value of this.subKeys;
+    }
+
+    //each round of DES (16 rounds)
+    private BitSet round(BitSet bits, int roundnum){
+    }
+
     /**
-     * Takes input string and breaks it into blocks of 64-bits
+     * Takes input string and breaks it into blocks of 64-bits, stored in a String array where each entry represents 64-bits
      * @param inputString
-     * @return
+     * @return String array with each entry having 8 characters
      */
-    private static BitSet[] chunk64Bits(String inputString) {
+    private String[] stringTo64Bits(String inputString) {
         int length = (inputString.length() / 8) + 1;
-        BitSet[] output = new BitSet[length];
+        String[] output = new String[length];
         for(int i = 0; i < output.length; i++) {
-            output[i] = stringToBits(inputString.substring(i * 8, (i + 1) * 8));
+            output[i] = stringToBitString(inputString.substring(i * 8, (i + 1) * 8));
         }
         return output;
     }
 
     /**
-     * Converts inputString into a BitSet
+     * Converts inputString into a char array
      * @param inputString
-     * @return
+     * @return a String as represented in binary via ASCII values of each character (64 digits long)
      */
-    private static BitSet stringToBits(String inputString) {
-        BitSet outputBits = new BitSet();
+    private String stringToBitString(String inputString) {
+        StringBuilder sbr = new StringBuilder();
         for(char c : inputString.toCharArray()) {
-
+            int asciiValue = c;
+            String binaryValue = Integer.toBinaryString(asciiValue);
+            if(binaryValue.length() < 8) {
+                for(int j = 0; j < 8 - binaryValue.length(); j++) {
+                    sbr.append("0");
+                }
+            }
+            sbr.append(binaryValue);
         }
+
+        return sbr.toString();
     }
 
+
+    //caroline
     //TODO: write method
     /**
      * f function, which applies a 48-bit key to the right 32 bits to produce a 32 bit output
--- RSA.java
+++ RSA.java
@@ -1,3 +1,5 @@
+package security;
+
 import java.math.BigInteger;
 import java.util.Random;
 
