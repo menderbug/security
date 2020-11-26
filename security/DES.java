271730766623444a5be16d98d98f572061716ea9
Deepa <deepabhuv@gmail.com>
2020-11-26 02:36:40 -0600

    added leftCircularShift and encrypt method. edited sbox and generateSubKeys methods

--- security/DES.java
+++ security/DES.java
@@ -24,7 +24,49 @@ public class DES {
                                         2, 8, 24, 14, 32, 27, 3, 9,
                                         19, 13, 30, 6, 22, 11, 4, 25};
     //TODO: the rest of these sboxes
-    private static final int[][] sbox = {};
+    private final int[][] sbox = {
+      		 {
+      		        14, 4,  13, 1,  2,  15, 11, 8,  3,  10, 6,  12, 5,  9,  0,  7,
+      		        0,  15, 7,  4,  14, 2,  13, 1,  10, 6,  12, 11, 9,  5,  3,  8,
+      		        4,  1,  14, 8,  13, 6,  2,  11, 15, 12, 9,  7,  3,  10, 5,  0,
+      		        15, 12, 8,  2,  4,  9,  1,  7,  5,  11, 3,  14, 10, 0,  6,  13
+      		    }, {
+      		        15, 1,  8,  14, 6,  11, 3,  4,  9,  7,  2,  13, 12, 0,  5,  10,
+      		        3,  13, 4,  7,  15, 2,  8,  14, 12, 0,  1,  10, 6,  9,  11, 5,
+      		        0,  14, 7,  11, 10, 4,  13, 1,  5,  8,  12, 6,  9,  3,  2,  15,
+      		        13, 8,  10, 1,  3,  15, 4,  2,  11, 6,  7,  12, 0,  5,  14, 9
+      		    }, {
+      		        10, 0,  9,  14, 6,  3,  15, 5,  1,  13, 12, 7,  11, 4,  2,  8,
+      		        13, 7,  0,  9,  3,  4,  6,  10, 2,  8,  5,  14, 12, 11, 15, 1,
+      		        13, 6,  4,  9,  8,  15, 3,  0,  11, 1,  2,  12, 5,  10, 14, 7,
+      		        1,  10, 13, 0,  6,  9,  8,  7,  4,  15, 14, 3,  11, 5,  2,  12
+      		    }, {
+      		        7,  13, 14, 3,  0,  6,  9,  10, 1,  2,  8,  5,  11, 12, 4,  15,
+      		        13, 8,  11, 5,  6,  15, 0,  3,  4,  7,  2,  12, 1,  10, 14, 9,
+      		        10, 6,  9,  0,  12, 11, 7,  13, 15, 1,  3,  14, 5,  2,  8,  4,
+      		        3,  15, 0,  6,  10, 1,  13, 8,  9,  4,  5,  11, 12, 7,  2,  14
+      		    }, {
+      		        2,  12, 4,  1,  7,  10, 11, 6,  8,  5,  3,  15, 13, 0,  14, 9,
+      		        14, 11, 2,  12, 4,  7,  13, 1,  5,  0,  15, 10, 3,  9,  8,  6,
+      		        4,  2,  1,  11, 10, 13, 7,  8,  15, 9,  12, 5,  6,  3,  0,  14,
+      		        11, 8,  12, 7,  1,  14, 2,  13, 6,  15, 0,  9,  10, 4,  5,  3
+      		    }, {
+      		        12, 1,  10, 15, 9,  2,  6,  8,  0,  13, 3,  4,  14, 7,  5,  11,
+      		        10, 15, 4,  2,  7,  12, 9,  5,  6,  1,  13, 14, 0,  11, 3,  8,
+      		        9,  14, 15, 5,  2,  8,  12, 3,  7,  0,  4,  10, 1,  13, 11, 6,
+      		        4,  3,  2,  12, 9,  5,  15, 10, 11, 14, 1,  7,  6,  0,  8,  13
+      		    }, {
+      		        4,  11, 2,  14, 15, 0,  8,  13, 3,  12, 9,  7,  5,  10, 6,  1,
+      		        13, 0,  11, 7,  4,  9,  1,  10, 14, 3,  5,  12, 2,  15, 8,  6,
+      		        1,  4,  11, 13, 12, 3,  7,  14, 10, 15, 6,  8,  0,  5,  9,  2,
+      		        6,  11, 13, 8,  1,  4,  10, 7,  9,  5,  0,  15, 14, 2,  3,  12
+      		    }, {
+      		        13, 2,  8,  4,  6,  15, 11, 1,  10, 9,  3,  14, 5,  0,  12, 7,
+      		        1,  15, 13, 8,  10, 3,  7,  4,  12, 5,  6,  11, 0,  14, 9,  2,
+      		        7,  11, 4,  1,  9,  12, 14, 2,  0,  6,  10, 13, 15, 3,  5,  8,
+      		        2,  1,  14, 7,  4,  10, 8,  13, 15, 12, 9,  0,  3,  5,  6,  11
+      		    } 
+      };
     private static final int[] ebox = {32, 1, 2, 3, 4, 5,
                                         4, 5, 6, 7, 8, 9,
                                         8, 9, 10, 11, 12, 13,
@@ -64,7 +106,34 @@ public class DES {
     //TODO: write encode method
     public BitSet encrypt(String encryptText) {
         BitSet[] blockStrings = stringTo64Bits(encryptText);
-        return new BitSet();
+        BitSet[] bit = permutation(blockStrings,ipbox); //inital permutation
+        
+        //left side (first 32 bits) 
+        BitSet Left = new BitSet();
+        bits.stream().filter(a -> a < 32).forEach(Left::set);
+        
+        //right side (last 32 bits)
+        BitSet Right = new BitSet();
+        bits.stream().filter(a -> a >= 32).forEach(a -> Right.set(a - 32));
+        for(int iter=0; iter<16; iter++) {
+        	
+           Left.xor(round(bit));
+           
+           //Right side bits are switched to the left side and vice versa to set up for next round 
+           temp = Right;
+           Right = Left;
+           Left = temp;
+           
+        }
+        //switch Left and Right bits again
+        temp = Right;
+        Right = Left;
+        Left = temp;
+        
+        bits = permutation(bits, inverseipbox); //inverse permutation
+        return bits;
+        //return new BitSet();
+        
     }
 
     // for triple DES, needs to be able to encrypt output
@@ -176,6 +245,9 @@ public class DES {
     private BitSet f(BitSet input) {
         BitSet bits = permutation(input, ebox);
         bits.xor(subKeys[iteration]);
+        bits = sboxTransform(bits,sbox);
+        bits = permutation(bits,pbox);
+        
         return bits;
     }
 
@@ -190,7 +262,27 @@ public class DES {
 
     //Deepa
     private BitSet sboxTransform(BitSet bits, int[] sbox) {
-        return new BitSet();
+    	int row = 0;
+		int column = 0;
+		
+		
+		for(int index = 0; index<bitlength+1; index+=6) {
+			
+			//gets first and last bit -> converts them into a string -> adds them into one string
+			String rowbits = Integer.toString(bits.get(index) ? 1 : 0) + Integer.toString(bits.get(index+5) ? 1 : 0);
+			row = Integer.parseInt(rowbits,2);
+			
+			//gets middle 4 bits between first and last index and converts it to a string
+			String columnbits = "";
+			for(int j = index+1; j<index+5; j++) { // how to make it without for loop?
+				columnbits += Integer.toString(bits.get(j) ? 1 : 0);
+			}
+			column = Integer.parseInt(columnbits,2);
+			System.out.println(row + " ROW; " + column + " COLUMN");
+			int x = sbox[roundnum][row*16];
+		}
+    	
+		return bits;
     }
     //Deepa
     private BitSet reverseSBoxTransform(BitSet bits, int[] sbox) {
@@ -199,14 +291,39 @@ public class DES {
 
     //Deepa
     private BitSet[] generateSubkeys(BitSet bits){
+    	for(int i = 0; i<16; i++) {
+    		//left side is the first 28 bits
+            BitSet Left = new BitSet();
+            bits.stream().filter(a -> a < 28).forEach(Left::set);
+
+            //right side is the last 28 bits
+            BitSet Right = new BitSet();
+            bits.stream().filter(a -> a >= 28).forEach(a -> Right.set(a - 28));
+           
+            for(roundnum=0; roundnum<iteration; roundnum++) {
+            	Left = leftCircularShift(Left, roundnum);
+                Right = leftCircularShift(Right, roundnum);
+            }
+            permutation
+            
+    	}
+    	
+		return subKeys;
         //do the 16 rounds and change value of this.subKeys;
-        return new BitSet[1];
+        //return new BitSet[1];
     }
 
     //Deepa
     private BitSet leftCircularShift (BitSet bits,  int roundnum) {
-        //can pull the amount of shifts to make from this.numShifts
-        return new BitSet();
+    	int shift = this.numShifts[iteration];
+    	BitSet temp = bits.get(shift, bits.length());
+    	
+    	for(int i = 1; i<shift+1; i++) {
+        	temp.set(bits.length()-i-1, bits.get(i-1));    		
+    	}
+    	
+		return bits;
+        //return new BitSet();
     }
 
     /**
