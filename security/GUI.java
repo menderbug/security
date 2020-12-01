584dabad523babdb6f80f9192876f719d620294b
David Ye <dly8@case.edu>
2020-11-30 23:50:41 -0500

    added tabs to gui

--- security/GUI.java
+++ security/GUI.java
@@ -2,6 +2,8 @@ package security;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
@@ -10,7 +12,7 @@ import javafx.stage.Stage;
 
 public class GUI extends Application {
 
-	private GridPane board = new GridPane();
+	private TabPane pane = new TabPane();
 
 	public static void main(String[] args) {launch(args);}
 
@@ -18,7 +20,7 @@ public class GUI extends Application {
 
 		DES des = new DES();
 		RSA rsa = new RSA();
-		Scene primaryScene = new Scene(board);
+		Scene primaryScene = new Scene(pane);
 		primaryStage.setScene(primaryScene);
 
 
@@ -40,52 +42,58 @@ public class GUI extends Application {
 		});
 
 		//DES Implementation
-		TextArea DESInput = new TextArea("all the girls with heads inside a dream so now we live beside the pool where everything is good");
-		TextArea DESKey = new TextArea(des.keyToNums());
-		String encryptedtext  = DES.bitsetToString(des.encrypt(DESInput.getText()), (int) (Math.ceil((double) DESInput.getText().length() / 8)) * 64);
-		TextArea DESEncryptedText = new TextArea(encryptedtext);
+		TextArea desInput = new TextArea("all the girls with heads inside a dream so now we live beside the pool where everything is good");
+		TextArea desKey = new TextArea(des.keyToNums());
+		String encryptedtext  = DES.bitsetToString(des.encrypt(desInput.getText()), (int) (Math.ceil((double) desInput.getText().length() / 8)) * 64);
+		TextArea desEncryptedText = new TextArea(encryptedtext);
 		TextArea rsaInput = new TextArea("all the girls with heads inside a dream so now we live beside the pool where everything is good");
 		TextArea rsaKey = new TextArea(RSA.getPublicKey());
 		TextArea rsaOutput = new TextArea(RSA.encrypt(rsaInput.getText()).toString());
 
+		GridPane vigenerePane = new GridPane();
+		GridPane desPane = new GridPane();
+		GridPane rsaPane = new GridPane();
+		
 
-		board.add(new TextField("Vigenere Encryption"), 0, 0, 4, 1);
-		board.add(new TextField("Text"), 0, 1);
-		board.add(new TextField("Key"), 1, 1);
-		board.add(new TextField("Encrypted Text"), 2, 1);
+		vigenerePane.add(new TextField("Vigenere Encryption"), 0, 0, 4, 1);
+		vigenerePane.add(new TextField("Text"), 0, 1);
+		vigenerePane.add(new TextField("Key"), 1, 1);
+		vigenerePane.add(new TextField("Encrypted Text"), 2, 1);
 
-		board.add(new TextField("DES Encryption"), 0, 3, 5, 1);
-		board.add(new TextField("Text"), 0, 4);
-		board.add(new TextField("Key"), 1, 4);
-		board.add(new TextField("Encrypted Text"), 2, 4);
-		board.add(new TextField("Decrypted Text"), 0, 6);
+		desPane.add(new TextField("DES Encryption"), 0, 0, 5, 1);
+		desPane.add(new TextField("Decrypted Text"), 0, 1);
+		desPane.add(new TextField("Key"), 1, 1);
+		desPane.add(new TextField("Encrypted Text"), 2, 1);
 
+		rsaPane.add(new TextField("RSA Encryption"), 0, 0, 3, 1);
+		rsaPane.add(new TextField("Text"), 0, 1);
+		rsaPane.add(new TextField("Key"), 1, 1);
+		rsaPane.add(new TextField("Encrypted Text"), 2, 1);
 
-		board.add(new TextField("RSA Encryption"), 0, 6, 3, 1);
-		board.add(new TextField("Text"), 0, 7);
-		board.add(new TextField("Key"), 1, 7);
-		board.add(new TextField("Encrypted Text"), 2, 7);
+		vigenerePane.add(vigenereInput, 0, 2);
+		vigenerePane.add(vigenereKey, 1, 2);
+		vigenerePane.add(vigenereOutput, 2, 2);
 
-		board.add(vigenereInput, 0, 2);
-		board.add(vigenereKey, 1, 2);
-		board.add(vigenereOutput, 2, 2);
+		desPane.add(desInput, 0, 2);
+		desPane.add(desKey, 1, 2);
+		desPane.add(desEncryptedText, 2, 2);
 
-		board.add(DESInput, 0, 5);
-		board.add(DESKey, 1, 5);
-
-		board.add(DESEncryptedText, 2, 5);
-
-		board.add(rsaInput, 0, 8);
-		board.add(rsaKey, 1, 8);
-		board.add(rsaOutput, 2, 8);
-
-		board.getChildren().forEach(n -> {if (n instanceof TextField) ((TextField) n).setEditable(false);});
-		board.getChildren().forEach(n -> {if (n instanceof TextArea) ((TextArea) n).setWrapText(true);;});
-
+		rsaPane.add(rsaInput, 0, 2);
+		rsaPane.add(rsaKey, 1, 2);
+		rsaPane.add(rsaOutput, 2, 2);
+		
+		pane.getTabs().add(new Tab("Vigenere", vigenerePane));
+		pane.getTabs().add(new Tab("DES", desPane));
+		pane.getTabs().add(new Tab("RSA", rsaPane));
+		
+		pane.getTabs().forEach(t -> {
+			((GridPane) t.getContent()).getChildren().forEach(n -> {if (n instanceof TextField) ((TextField) n).setEditable(false);});
+			((GridPane) t.getContent()).getChildren().forEach(n -> {if (n instanceof TextArea) ((TextArea) n).setWrapText(true);;});
+		});
 
 		primaryStage.setTitle("Encoder/Decoder");
 		//primaryStage.initStyle(StageStyle.UNDECORATED);
-		primaryScene.getStylesheets().add(getClass().getResource("gui.css").toExternalForm());
+		primaryScene.getStylesheets().add("gui.css");
 		primaryStage.setWidth(Screen.getPrimary().getVisualBounds().getWidth());
 		primaryStage.sizeToScene();
 		primaryStage.show();
