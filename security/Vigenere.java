849443c08343ca7aedd7b89799d95dcda95a755c
David Ye <dly8@case.edu>
2020-12-12 16:06:44 -0500

    small fix to vigenere, whitespace handling

--- security/Vigenere.java
+++ security/Vigenere.java
@@ -4,10 +4,6 @@ import java.util.function.BiFunction;
 
 public class Vigenere {
 	
-	public static void main(String[] args) {
-		System.out.println(Vigenere.decrypt("qmflivo", "eecs"));
-	}
-	
 	//shifts message forward by characters in key
 	public static String encrypt(String message, String key) {
 		return shift(message, key, (original, shift) -> (char) ((original + shift - (2 * 'a')) % 26 + 'a'));
@@ -20,11 +16,12 @@ public class Vigenere {
 
 	//applies a function to characters in message and key to produce a shift
 	public static String shift(String message, String key, BiFunction<Character, Character, Character> bf) {
+		if (key.isEmpty()) return message;
 		StringBuilder sb = new StringBuilder(message);
 		key = key.toLowerCase();
-		for (int i = 0; i < message.length(); i++)
+		for (int i = 0, j = 0; i < message.length(); i++)
 			if (Character.isLetter(sb.charAt(i)))
-				sb.setCharAt(i, bf.apply(sb.charAt(i), key.charAt(i % key.length())));
+				sb.setCharAt(i, bf.apply(sb.charAt(i), key.charAt((j++) % key.length())));
 		return sb.toString();
 	}
 
